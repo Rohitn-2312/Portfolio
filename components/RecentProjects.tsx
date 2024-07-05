677c617dc7c9b8edf20579504f26bcea65a5840b
Rohit Nayak <rohitn822@gmail.com>
2024-07-05 14:33:59 +0530

    ui change

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-Pin'
 
 const RecentProjects = () => {
   return (
@@ -11,7 +12,18 @@ const RecentProjects = () => {
       <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
         {projects.map(({id, title, des,img,iconLists, link}) => (
             <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex itmes-center justify-center sm:w-96 w-[80vw]'>
-                {title}
+               <PinContainer title={title} href={link}>
+                   <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
+                    <div>
+                        <img src='/bg.png' alt='bg-img' />
+                    </div>
+                    <img 
+                    src={img}
+                    alt={title}
+                    className='z-10 absolute bottom-0'
+                    />
+                   </div>
+                </PinContainer>
             </div>
         ))}
       </div>
