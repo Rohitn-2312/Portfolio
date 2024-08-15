294b539971999d530d73ad6d62831566b92cb730
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-08-16 02:34:07 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -172,22 +172,22 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
+    title: "Participated in 10+ Hackathons",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Contributor @GSsOC'23",
+    desc: "Designed and developed websited using ReactJs and NextJs.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Google Cloud Career Practitioner",
+    desc: "Worked with google cloud and kubernetes. ",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
